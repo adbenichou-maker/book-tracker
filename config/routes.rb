8fa6433257fa1211b44eac3b64a403164e923cde
Adam Benichou <adbenichou@gmail.com>
2026-02-03 23:09:02 +0100

    edit route removed

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,5 @@ Rails.application.routes.draw do
   get "/new", to: "books#new", as: "new_book"
   post "/create", to: "books#create"
   patch "books/:id/update_status", to: "books#update_status", as: "update_book_status"
-  get "books/:id/edit", to: "books#edit", as: "edit_book"
-  patch "books/:id", to: "books#update"
   delete "books/:id", to: "books#destroy" , as: "delete_book"
 end
